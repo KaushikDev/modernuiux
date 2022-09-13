a45e0bd368dcca4398f346d8c153be2f4b309e31
kaushikDev <piyush.kaushik02@gmail.com>
2022-09-13 21:29:39 +0530

    updated code

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,7 +1,75 @@
+import { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
+const Menu = () => {
+  return (
+    <>
+      <p>
+        <a href="#home">Home</a>
+      </p>
+      <p>
+        <a href="#wai">What is AI?</a>
+      </p>
+      <p>
+        <a href="#possibility">Open AI</a>
+      </p>
+      <p>
+        <a href="#features">Case Studies</a>
+      </p>
+      <p>
+        <a href="#blog">Library</a>
+      </p>
+    </>
+  );
+};
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [toggleMenu, setToggleMenu] = useState(false);
+  return (
+    <div className="ai__navbar">
+      <div className="ai__navbar-links">
+        <div className="ai__navbar-links_logo">
+          <img src={logo} alt="" />
+        </div>
+        <div className="ai__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="ai__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="ai__navbar-menu">
+        {toggleMenu ? (
+          <RiCloseLine
+            size={27}
+            color={"#fff"}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            size={27}
+            color={"#fff"}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
+        {toggleMenu && (
+          <div className="ai__navbar-menu_container scale-up-center">
+            <div className="ai__navbar-menu_container-links">
+              <Menu />
+              <div className="ai__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,7 +1,39 @@
 import "./header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="ai__header section__padding" id="home">
+      <div className="ai__header-content">
+        <h1 className="gradient__text">
+          Let's build something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+          unde quo, voluptate ea perferendis, aut iure sunt laudantium, in
+          exercitationem nisi accusantium iusto. Beatae harum doloribus
+          explicabo sapiente necessitatibus aliquid maxime ea neque animi,
+          minima aperiam vero cumque, laboriosam maiores veritatis autem nam
+          quibusdam tempora nihil qui fugiat.
+        </p>
+        <div className="ai__header-content__input">
+          <input type="email" placeholder="Your email address" />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="ai__header-content__people">
+          <img src={people} alt="people" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae
+            unde quo.
+          </p>
+        </div>
+      </div>
+      <div className="ai__header-image">
+        <img src={ai} alt="ai" />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
